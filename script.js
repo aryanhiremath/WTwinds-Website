7779ef8f344554302eeff5c891c7f88cb25b4567
aryanhiremath <aryanhiremath14@gmail.com>
2026-01-07 18:15:58 +0530

    Initial website upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const mobileMenu = document.getElementById('mobile-menu');
+    const navList = document.getElementById('nav-list');
+
+    mobileMenu.addEventListener('click', () => {
+        navList.classList.toggle('show');
+    });
+});
